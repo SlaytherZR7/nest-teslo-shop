e3ffcf81bbd56a0de1d82e62909028adcd36f744
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-08-10 20:26:34 -0500

    upload files

---
+++ src/files/files.service.ts
@@ -0,0 +1,14 @@
+import { join } from 'path';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { existsSync } from 'fs';
+
+@Injectable()
+export class FilesService {
+  getStaticProductImage(imageName: string) {
+    const path = join(__dirname, '../../static/products', imageName);
+    if (!existsSync(path)) {
+      throw new NotFoundException(`No image found with name ${imageName}`);
+    }
+    return path;
+  }
+}
